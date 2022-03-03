1a4dcc92c31cd5ac8b1544ccd4b40bc282bd7920
Luiz <luiz.contes@hotmail.com>
2022-03-03 12:05:37 +0000

    More small scripts used to master C programming

--- headFirstExecChap11/connectTest.c
+++ headFirstExecChap11/connectTest.c
@@ -26,7 +26,7 @@ void bind_to_port(int socket, int port) {
 
     struct sockaddr_in name;
     name.sin_family = PF_INET;
-    name.sin_port = (in_port_t)htons(4000);
+    name.sin_port = (in_port_t)htons(30000);
     name.sin_addr.s_addr = htonl(INADDR_ANY);
     int reuse = 1;
     if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse, sizeof(int)) == -1)
